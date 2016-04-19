a3cd975ba1873d73791cc3da217708e28d363c7d
Michael Peters <opiethehokie@gmail.com>
2016-04-19 16:09:11 -0400

    remove doc links that still point at wrong location

--- app/index.php
+++ app/index.php
@@ -13,9 +13,7 @@
 			<td>
 				<h1 id = "message"><?php echo "Hello world!"; ?>
 </h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>. Get started by reading our <a
-				href="https://www.ng.bluemix.net/docs/#runtimes/php/index.html">documentation</a>
-				or use the Start Coding guide under your app in your dashboard.
+				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>. To get started see the Start Coding guide under your app in your dashboard.
 			</td>
 		</tr>
 	</table>
